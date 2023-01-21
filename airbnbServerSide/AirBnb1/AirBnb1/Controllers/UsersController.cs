d09f2748cbf7786fb37460877d975542e2155543
rupcgroup30 <rupcgroup30@gmail.com>
2023-01-22 00:14:55 +0200

    Initial commit

---
+++ airbnbServerSide/AirBnb1/AirBnb1/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using AirBnb1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AirBnb1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<User> Get(string email, string password)
+        {
+            User user=new User();   
+            return user.Read(email, password);
+        }
+
+        // GET api/<FlatsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("/GetAllUsers")]
+        public List<User> GetAllUsers()
+        {
+            User user = new User();
+            return user.ReadAllUsers();
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public int Post([FromBody] User user)
+        {
+            return user.Insert();
+
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut]
+        public int Put([FromBody] User user)
+        {
+            return user.Update();
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ airbnbServerSide/AirBnb1/AirBnb1/Models/User.cs
@@ -0,0 +1,45 @@
+﻿namespace AirBnb1.Models
+{
+    public class User
+    {
+        public static List<User> UsersList=new List<User>();
+
+        private string firstName;
+        private string lastName;
+        private string password;
+        private string email;
+        private bool isActive;
+        private bool isAdmin;
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+        public bool IsActive { get => isActive; set => isActive = value; }
+        public bool IsAdmin { get => isAdmin; set => isAdmin = value; }
+
+        public List<User> Read(string email, string password)
+        {
+            DBservices dbs=new DBservices();
+            return dbs.ReadUser(email, password);
+        }
+
+        public List<User> ReadAllUsers()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.ReadAllUsers();
+        }
+
+        public int Insert()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.InsertUser(this);
+        }
+
+        public int Update()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.UpdateUser(this);
+        }
+
+    }
+}
